e03e9815567bc099d4b0cc70af9466faa1d5fd5d
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-12-20 10:23:37 +0100

    add contactForm component to use emailjs

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,26 +1,15 @@
-import { useState } from "react";
 import "./styles.scss";
+import ContactForm from '../ContactForm';
 
 export default function Contact() {
-  const [message, setMessage] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setMessage(true);
-  };
   return (
     <div className="contact" id="contact">
       <div className="left">
         <img src="assets/shake.svg" alt="" />
       </div>
       <div className="right">
-        <h2>Contact.</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
-          <button type="submit">Send</button>
-          {message && <span>Thanks, I'll reply ASAP :)</span>}
-        </form>
+        <ContactForm />
       </div>
     </div>
   );
